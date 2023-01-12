e32e9f4112128c4077651bac00858f06fdc00c04
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-12 20:27:47 +0200

    Select One item using Find

---
+++ EF Core/Select One item using Find.cs
@@ -0,0 +1,18 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Select One item using Find
+            var _context = new ApplicationDbContext();
+
+            var stock = _context.Stocks.Find(1);
+            Console.WriteLine($"Id: {stock.Id}: {stock.Name}");
+            #endregion
+        }
+
+    }
+}
