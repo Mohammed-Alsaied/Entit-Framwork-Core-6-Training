c2db824ce0185cc8fb1a2b2a491b77f1aedd453f
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-12 20:27:36 +0200

    Select All Data

---
+++ EF Core/Select All Data.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    //static void Main(string[] args)
+    //{
+    //    #region Select All Data
+    //    //var _context = new ApplicationDbContext();
+
+    //    //var stocks = _context.Stocks.ToList();
+
+    //    //foreach (var stock in stocks)
+    //    //{
+    //    //    Console.WriteLine(stock.Name);
+    //    //}
+    //    #endregion
+    //}
+
+}
