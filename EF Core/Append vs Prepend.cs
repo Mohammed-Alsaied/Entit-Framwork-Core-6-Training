d3802e317e4035dd97894dbd7f990c0dd9e42839
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-12 23:13:33 +0200

    Append vs Prepend

---
+++ EF Core/Append vs Prepend.cs
@@ -0,0 +1,33 @@
+﻿using EF_Core.DbContexts;
+using EF_Core.Entities;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Append vs Prepend
+            var _context = new ApplicationDbContext();
+
+            #region Append
+            var stocks = _context.Stocks.Where(s => s.Id > 900).ToList().Append(new Stock { Id = 1001, Name = "test" });//add data last of list
+            foreach (var stock in stocks)
+            {
+                Console.WriteLine($"Id:{stock.Id}:Name:{stock.Name}");
+            }
+            #endregion Append
+
+            #region Prepend
+            var stocks = _context.Stocks.Where(s => s.Id > 900).ToList().Prepend(new Stock { Id = 1001, Name = "test" });//add data First of list
+            foreach (var stock in stocks)
+            {
+                Console.WriteLine($"Id:{stock.Id}:Name:{stock.Name}");
+            }
+            #endregion Prepend
+
+            #endregion Any vs All
+        }
+
+    }
+}
