13a0437641271116319ee695ddda6ba97493e0c7
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-12 21:55:28 +0200

    Select One Item Using Single

---
+++ EF Core/Select One Item Using Single.cs
@@ -0,0 +1,18 @@
+﻿namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Select One Item Using Single
+            var _context = new ApplicationDbContext();
+            var stock = _context.Stocks.Single();//only one item
+            var stock1 = _context.Stocks.Single(s => s.Id == 100);
+            Console.WriteLine($"Id: {stock1.Id}: {stock1.Name}");
+            var stock2 = _context.Stocks.SingleOrDefault(s => s.Id == 10000);
+            Console.WriteLine(stock2 == null ? "Item not found!" : $"Id: {stock2.Id}: {stock2.Name}");
+            #endregion
+        }
+
+    }
+}
