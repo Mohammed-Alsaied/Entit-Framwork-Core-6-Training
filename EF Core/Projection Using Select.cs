298c06f4ab3b80666d2afa1d02700e3acb280500
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-28 22:33:11 +0200

    Projection Using Select

---
+++ EF Core/Projection Using Select.cs
@@ -0,0 +1,31 @@
+﻿using EF_Core.DbContexts;
+using EF_Core.Entities;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Projection Using Select
+            var _context = new ApplicationDbContext();
+
+            #region Projection Using Select by Anonymous Object
+            var stocks = _context.Stocks.Select(s => new { StockId = s.Id, StockName = s.Name }).ToList();
+            var stocks = _context.Stocks.Select(s => new { s.Id, s.Name }).ToList();
+
+            #endregion Projection Using Select by Anonymous Object
+
+            #region Projection Using Select by Another Object
+
+            var stocks = _context.Stocks.Select(s => new Blog { Id = s.Id, Url = s.Name }).ToList();
+
+            #endregion Projection Using Select by Another Object
+
+            foreach (var stock in stocks)
+                Console.WriteLine($"Id: {stock.Id}:{stock.Url}");
+            #endregion Projection Using Select
+        }
+
+    }
+}
