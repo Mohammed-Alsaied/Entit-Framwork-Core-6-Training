3ceddd9956ae3c33a2dc2aa4c62c1878b7729cfd
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-28 21:53:04 +0200

    Max vs Min

---
+++ EF Core/Max vs Min.cs
@@ -0,0 +1,32 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Max vs Min
+            var _context = new ApplicationDbContext();
+
+            #region Max
+            var stocks = _context.Stocks.Max(s => s.Balance);
+            var stocks1 = _context.Stocks.Max(s => s.Id);
+            stocks = _context.Stocks.Max(s => s.Name);
+            Console.WriteLine(stocks);
+            Console.WriteLine(stocks1);
+            #endregion Max
+
+            #region Min
+            var stocks2 = _context.Stocks.Min(s => s.Balance);
+            var stocks3 = _context.Stocks.Min(s => s.Id);
+            stocks2 = _context.Stocks.Min(s => s.Name);
+            Console.WriteLine(stocks2);
+            Console.WriteLine(stocks3);
+            #endregion Min
+
+            #endregion Max vs Min
+        }
+
+    }
+}
