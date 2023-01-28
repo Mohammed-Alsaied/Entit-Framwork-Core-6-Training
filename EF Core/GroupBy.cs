030973d9dab9381704eb3ca6fb61e425e25b556a
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-28 23:06:20 +0200

    GroupBy

---
+++ EF Core/GroupBy.cs
@@ -0,0 +1,27 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region GroupBy
+
+            var _context = new ApplicationDbContext();
+
+            var Industries = _context.Stocks
+                .GroupBy(x => x.Industry)
+                .Select(m => new { Name = m.Key, StockCount = m.Count(), BalanceSum = m.Sum(m => m.Balance), BalanceAverage = m.Average(x => x.Balance) })
+                .OrderByDescending(m => m.StockCount);
+
+            foreach (var Industry in Industries)
+                Console.WriteLine($" {Industry.Name} - {Industry.StockCount} - {Industry.BalanceSum} - {Industry.BalanceAverage}");
+            #endregion  GroupBy
+        }
+
+
+    }
+
+
+}
