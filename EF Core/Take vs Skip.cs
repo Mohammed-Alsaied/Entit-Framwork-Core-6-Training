e0d85a409daf60a690f33a509847ab855e35e554
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-28 22:51:24 +0200

    Take vs Skip

---
+++ EF Core/Take vs Skip.cs
@@ -0,0 +1,41 @@
+﻿using EF_Core.DbContexts;
+using EF_Core.Entities;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Take vs Skip
+
+            #region Take
+            var _context = new ApplicationDbContext();
+
+            var stocks = GetData(5, 20);
+
+            var stocks = _context.Stocks.Skip(0).Take(10).ToList();
+
+            #endregion Take
+
+            #region Skip
+            var stocks = _context.Stocks.Skip(10).Take(10).ToList();
+
+
+            #endregion Skip
+
+            foreach (var stock in stocks)
+                Console.WriteLine($"Id: {stock.Id}");
+            #endregion  Take vs Skip
+        }
+
+        public static List<Stock> GetData(int pageNumber, int pageSize)
+        {
+            var _context = new ApplicationDbContext();
+
+            return _context.Stocks.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        }
+    }
+
+
+}
