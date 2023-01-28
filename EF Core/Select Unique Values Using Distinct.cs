96171310b3c11e892ce741ef20f11021905dd060
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-28 22:39:56 +0200

    Select Unique Values Using Distinct

---
+++ EF Core/Select Unique Values Using Distinct.cs
@@ -0,0 +1,22 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Select Unique Values Using Distinct
+            var _context = new ApplicationDbContext();
+
+            var stocks = _context.Stocks.Select(s => new { StockId = s.Id, StockName = s.Name }).Distinct().ToList();
+            var stocks = _context.Stocks.Select(s => new { s.Industry }).Distinct().ToList();
+
+            foreach (var stock in stocks)
+                Console.WriteLine($"Id: {stock.StockId}:{stock.StockName}");
+            Console.WriteLine($"Id: {stock.Industry}");
+            #endregion  Select Unique Values Using Distinct
+        }
+
+    }
+}
