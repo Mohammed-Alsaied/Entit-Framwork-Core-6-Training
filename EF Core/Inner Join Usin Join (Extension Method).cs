26121aa693379752e0d32e3cee1faac0cce3cfee
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-01-31 18:54:38 +0200

    Inner Join Usin Join (Extension Method)

---
+++ EF Core/Inner Join Usin Join (Extension Method).cs
@@ -0,0 +1,42 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _context = new ApplicationDbContext();
+
+            var books = _context.Books
+                .Join
+                (
+                    _context.Authors,
+                    book => book.AuthorId,
+                    author => author.Id,
+                    (book, author) => new
+                    {
+                        BookId = book.Id,
+                        BookName = book.Name,
+                        AuthorName = author.Name,
+                        AuthorNationalityId = author.NationalityId
+                    }
+                )
+                .Join
+                (
+                    _context.Nationalities,
+                    book => book.AuthorNationalityId,
+                   nationality => nationality.Id,
+                   (book, nationality) => new
+                   {
+                       book.BookId,
+                       book.BookName,
+                       book.AuthorName,
+                       AuthorNationality = nationality.Name
+                   }
+                );
+            foreach (var book in books)
+                Console.WriteLine($"{book.BookId} - {book.BookName} - {book.AuthorName} - {book.AuthorNationality}");
+        }
+    }
+}
