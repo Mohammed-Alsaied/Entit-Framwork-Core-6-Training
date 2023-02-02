e5ca968734b5d7b2eec260a33a27b63ac45420fd
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-02-03 00:01:25 +0200

    Join Using LINQ

---
+++ EF Core/Join Using LINQ.cs
@@ -0,0 +1,48 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _context = new ApplicationDbContext();
+
+            #region Inner join
+            var booksInnerjoin = (from b in _context.Books
+                                  join a in _context.Authors
+                                  on b.AuthorId equals a.Id
+                                  join n in _context.Nationalities
+                                  on a.NationalityId equals n.Id
+                                  select new
+                                  {
+                                      BookId = b.Id,
+                                      BookName = b.Name,
+                                      AuthorName = a.Name
+
+                                  }).ToList();
+            //}).Cpunt();
+
+            #endregion Inner join
+
+            #region Left join
+            var booksLeftjoin = (from b in _context.Books
+                                 join a in _context.Authors
+                                 on b.AuthorId equals a.Id
+                                 join n in _context.Nationalities
+                                 on a.NationalityId equals n.Id into authorNationality
+                                 from an in authorNationality.DefaultIfEmpty()
+
+                                 select new
+                                 {
+                                     BookId = b.Id,
+                                     BookName = b.Name,
+                                     AuthorName = a.Name
+
+                                 }).ToList();
+            //}).Cpunt();
+            #endregion Left join
+
+        }
+    }
+}
--- EF Core/Split Queries.cs
+++ EF Core/Split Queries.cs
@@ -1,17 +1,14 @@
-﻿using EF_Core.DbContexts;
-using Microsoft.EntityFrameworkCore;
-
-namespace EF_Core
+﻿namespace EF_Core
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //using by Eager Loading only
-            var _context = new ApplicationDbContext();
-            var blog = _context.Blogs
-                .Include(b => b.Posts).AsSplitQuery().ToList();
-                .Include(b => b.Posts).AsSingleQuery().ToList();
-        }
-    }
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        //using by Eager Loading only
+    //        var _context = new ApplicationDbContext();
+    //        var blog = _context.Blogs
+    //            //.Include(b => b.Posts).AsSplitQuery().ToList();
+    //            .Include(b => b.Posts).AsSingleQuery().ToList();
+    //    }
+    //}
 }
