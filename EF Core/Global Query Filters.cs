265c4a554a3fc479ce7a96fbec0025ee7a954b20
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-02-03 22:55:12 +0200

    Global Query Filters

---
+++ EF Core/Global Query Filters.cs
@@ -0,0 +1,28 @@
+﻿using EF_Core.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _context = new ApplicationDbContext();
+
+            var postsWithGlobalQueryFilter = _context.Posts.ToList();//with Global HasQueryFilter
+            var postsWithoutGlobalQueryFilter = _context.Posts.Where(p => !p.IsDeleted).ToList();//without HasQueryFilter
+            foreach (var post in postsWithGlobalQueryFilter)
+            {
+                Console.WriteLine(post.Title);
+            }
+
+            var blogs = _context.Blogs.ToList();
+            var blogsWithIgnoreQureyFilters = _context.Blogs.IgnoreQueryFilters().ToList();
+            foreach (var blog in blogs)
+            {
+                Console.WriteLine(blog.Id);
+            }
+
+        }
+    }
+}
