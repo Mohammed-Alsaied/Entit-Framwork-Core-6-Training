6ad61bdae446457640e5a6b013e60f35474c2cec
Mohamed Alsaied <mohamed.saied.civil@gmail.com>
2023-02-21 22:26:28 +0200

    EF7 Updates - Bulk Delete_Bulk Update

---
+++ EF Core/EF7 Updates - Bulk Delete_Bulk Update.cs
@@ -0,0 +1,20 @@
+﻿using EF_Core.DbContexts;
+
+namespace EF_Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var _context = new ApplicationDbContext();
+
+            #region EF7 Updates - Bulk Delete_Bulk Update
+            _context.Blogs.Skip(950).ExecuteDelete();
+            _context.Blogs.Where(e => e.Id > 950).ExecuteDelete();
+
+            _context.Blogs.Where(e => e.Id > 900)
+                .ExecuteUpdate(p => p.SetProperty(e => e.Url, e => e.Url + " updated"));
+            #endregion EF7 Updates - Bulk Delete_Bulk Update
+        }
+    }
+}
